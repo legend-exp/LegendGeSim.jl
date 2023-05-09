b76b0c02727e0305a14e2b01880f3b1aac3497d6
Felix Hagemann <30291312+fhagemann@users.noreply.github.com>
2023-05-10 01:51:17 +0200

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,23 +7,27 @@ using LegendGeSim
 using LegendTestData
 using Unitful
 
-testdata_path = joinpath(LegendTestData.legend_test_data_path(), "data", "ldsim")
+testdata_path = joinpath(LegendTestData.legend_test_data_path(), "data", "legend", "metadata", "hardware", "detectors", "germanium", "diodes")
+
+test_dict = Dict{String, typeof(1.0u"pF")}(
+    "B99000A.json" => -7.13u"pF",
+    "V99000A.json" => -3.55u"pF"
+)
 
 @testset "Package LegendGeSim" begin
-    detector_metadata_filename = joinpath(testdata_path, "invcoax-metadata.json") # path to my detector metadata
-    sim_settings_ssd_filename = joinpath(dirname(dirname(pathof(LegendGeSim))), "examples/configs/SSD_NoiseSim.json")
+    for (filename, capacitance) in test_dict
+        detector_metadata_filename = joinpath(testdata_path, filename)
+        sim_settings_ssd_filename = joinpath(dirname(dirname(pathof(LegendGeSim))), "examples/configs/SSD_NoiseSim.json")
+        
+        @testset "Field Simulation of $filename (SSD)" begin
+            config = LegendGeSim.load_config(detector_metadata_filename, sim_settings_ssd_filename);
+            sim = LegendGeSim.simulate_fields(config)
+            C = LegendGeSim.capacitance_matrix(sim)
 
-    
-    @testset "Field Simulation (SSD)" begin
-        config = LegendGeSim.load_config(detector_metadata_filename, sim_settings_ssd_filename);
-
-        sim = LegendGeSim.simulate_fields(config)
-
-        C = LegendGeSim.capacitance_matrix(sim)
-
-        @testset "Capacitances" begin   
-            @test isapprox(ustrip(C[1,2]), -3.25, atol = 0.2)
+            @testset "Capacitances" begin   
+                @test isapprox(C[1,2], capacitance, atol = 0.2u"pF")
+            end
         end
-    end
-end 
+    end 
+end
 
