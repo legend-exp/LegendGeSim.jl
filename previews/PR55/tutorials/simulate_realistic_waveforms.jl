382179e6ca18f9425e0ce384913de6ed77f50a7b
Documenter.jl <documenter@juliadocs.github.io>
2026-09-11 14:53:42 +0000

    build based on 07adf32

---
+++ previews/PR55/tutorials/simulate_realistic_waveforms.jl
@@ -0,0 +1,89 @@
+using LegendGeSim
+using Plots
+
+using LegendTestData
+
+ldsim_path = joinpath(legend_test_data_path(), "data", "ldsim")
+
+detector_name = "invcoax-metadata"
+detector_metadata_filename = joinpath(ldsim_path, detector_name*".json");
+
+path_to_pet_file = joinpath(ldsim_path, "single-invcoax-th228-geant4.csv");
+
+environment_settings = Dict(
+    "crystal_temperature_in_K" => 77,
+    "medium" => "vacuum",
+);
+
+simulation_settings = Dict(
+    "method" => "SSD",
+    "cached_name" => "", # a non-empty string will cache the simulation results
+);
+
+daq_settings = Dict(
+    "preamp" => Dict(
+        "type" => "generic",
+        "t_decay_in_us" => 43, # from V04545A HADES data
+        "t_rise_in_ns" => 100, # by eye
+        "gain_ADC_eV" => 0.0138, # by eye from V04545A HADES data FEP @ 36100 ADC
+        "offset_in_ADC" => 11900, # from V04545A HADES data mean() of baseline
+        "noise_sigma_in_keV" => 2 # by eye
+    ),
+    "fadc" => Dict(
+        "type" => "generic",
+        "sampling_interval" => 16 # ns, from HADES data
+    ),
+    "trigger" => Dict(
+        "type" => "trapezoidal",
+        "window_lengths" => [250,250,250],
+        "threshold" => 9 # keV
+    ),
+    "daq" => Dict(
+        "type" => "generic",
+        "nsamples" => 3748, # from HADES data
+        "baseline_length" => 1770 # by eye from data
+    )
+);
+
+noise_model = Dict(
+    "type" => "sim"
+);
+
+raw_table = LegendGeSim.simulate_raw(detector_metadata_filename, path_to_pet_file, environment_settings, simulation_settings, daq_settings, noise_model; n_waveforms=10)
+
+plot(raw_table.waveform)
+
+length(raw_table)
+
+plot(raw_table.waveform[1:10], legend=false, title="only p+ contact waveforms")
+
+using HDF5
+using LegendHDF5IO
+
+raw_name = "cache/test_100wfs_raw.hdf5"
+h5open(raw_name, "w") do f
+    LegendHDF5IO.writedata(f, "raw", raw_table[1:10])
+end
+
+pss_table, pss_truth = LegendGeSim.simulate_pulses(detector_metadata_filename, path_to_pet_file, environment_settings, simulation_settings, noise_model; n_waveforms=10);
+
+plot(pss_table.waveform[1:10])
+
+raw_table1 = LegendGeSim.pss_to_raw(pss_table, pss_truth, daq_settings, noise_model)
+
+plot(
+    plot(pss_table.waveform[1:10]),
+    plot(raw_table1.waveform[1:10]),
+    size=(800,400)
+)
+
+using LegendHDF5IO
+using HDF5
+
+pss_name = "cache/test_100wfs_pss.hdf5"
+h5open(pss_name, "w") do f
+    LegendHDF5IO.writedata(f, "pss/pss", pss_table[1:10])
+    LegendHDF5IO.writedata(f, "pss/truth", pss_truth[1:10])
+end
+
+raw_table2 = LegendGeSim.pss_to_raw(pss_name, daq_settings, noise_model)
---
+++ v0.1.3/tutorials/simulate_ideal_pulses_lit.jl
@@ -0,0 +1,91 @@
+# # Simulate Ideal Pulses
+
+#md # You can also download this tutorial as a
+#md # [Jupyter notebook](simulate_ideal_pulses.ipynb) and a plain
+#md # [Julia source file](simulate_ideal_pulses.jl).
+
+# Here we will use an example `pet` file `csv` format from `LegendTestData` corresponding to the Public Inverted Coax.
+
+using LegendGeSim
+using Plots
+
+# ## Get inputs from `legend-test-data`
+
+using LegendTestData
+
+# ### Detector metadata
+
+ldsim_path = joinpath(legend_test_data_path(), "data", "ldsim")
+
+detector_name = "invcoax-metadata"
+detector_metadata_filename = joinpath(ldsim_path, detector_name*".json");
+
+# Alternatively, enter your own path to a real LEGEND detector JSON
+
+# ```julia
+# detector_metadata_filename = "path/to/V04545A.json"
+# ```
+
+# ### PET input file
+
+path_to_pet_file = joinpath(ldsim_path, "single-invcoax-th228-geant4.csv");
+
+# ## Settings
+
+# See manual on [Field Simulation](@ref) and [Ideal Pulse Simulation](@ref) for a detailed explanation of environment and simulation settings, as well as the noise model settings
+
+environment_settings = Dict(
+    "crystal_temperature_in_K" => 77, 
+    "medium" => "vacuum", 
+);
+
+# Simple settings for point charge simulation with dummy constant impurity
+
+simulation_settings = Dict(
+    "method" => "SSD",
+    "cached_name" => "", # a non-empty string will cache the simulation results
+);
+
+#
+
+noise_model = Dict(
+    "type" => "sim"
+);
+
+## Simulate ideal pulses
+
+# Provide an optional argument `n_waveforms` to define the number of pulses to be simulated. Default: all pulses based on the input file
+
+pss_table, pss_truth = LegendGeSim.simulate_pulses(detector_metadata_filename, path_to_pet_file, environment_settings, simulation_settings; n_waveforms=5);
+
+# 
+
+plot(pss_table.waveform, legend=false, linewidth=1.5)
+
+# The "negative" waveforms are the ones coming from the n+ contact, so they are symmetric. In fact, the length of our table is twice the amount of waveforms we simulated.
+
+length(pss_table)
+
+# 
+
+plot(pss_table.waveform[1:5], legend=false, title="only p+ contact pulses")
+
+#jl savefig("ideal_pulses.pdf") # hide
+#md savefig("ideal_pulses.svg"); nothing # hide
+#md # ![ideal_pulses](ideal_pulses.svg)
+
+# ## Save output to hdf5 file
+
+# You can save simulated pulses in a file that can be used later.
+
+using HDF5
+using LegendHDF5IO
+
+#
+
+pss_name = "cache/test_100wfs_pss.hdf5"
+h5open(pss_name, "w") do f
+    LegendHDF5IO.writedata(f, "pss/pss", pss_table[1:5])
+    LegendHDF5IO.writedata(f, "pss/truth", pss_truth[1:5])
+end
+
